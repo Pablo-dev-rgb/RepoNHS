70457c260e860764346fc4ce82cb063a64d34e72
Pablo-dev.rgb <villabeba777gmail.com>
2025-07-10 22:42:25 -0300

    intentando solucionar

--- app/Http/Controllers/Api/FrontController.php
+++ app/Http/Controllers/Api/FrontController.php
@@ -10,7 +10,7 @@ class FrontController extends Controller
 {
     public function notices(){
         $notices = Notice::orderByDesc("created_at")->paginate(
-            $perPage = 2, $colum = ['*'], $pageName = "notices"
+            $perPage = 3, $colum = ['*'], $pageName = "notices"
         );
         return response()->json($notices, 200);
     }
